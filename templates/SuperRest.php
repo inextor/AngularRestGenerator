68644dce11b3ab6432c1a20564517a88c9b2df36
nextor <nextor@gmail.com>
2020-01-02 01:30:15 -0800

    adding getStartLikeConstraints

--- templates/SuperRest.php
+++ templates/SuperRest.php
@@ -17,17 +17,6 @@ class SuperRest extends \akou\RestController
 		return $this->defaultOptions();
 	}
 
-	function getEqualConstraints($array,$table_name='')
-	{
-		$constraints = [];
-		foreach( $array as $index )
-		{
-			if( isset( $_GET[$index] ) && $_GET[$index] !== '' )
-				$constraints[] =($table_name?$table_name.'.':''). $index.'="'.DBTable::escape( $_GET[ $index ]).'"';
-		}
-		return $constraints;
-	}
-
 	function getPagination()
 	{
 		$page = 0;
@@ -43,7 +32,34 @@ class SuperRest extends \akou\RestController
 
 		return	$this->getPaginationInfo($page,$page_size,20);
 	}
+	function getEqualConstraints($array,$table_name='')
+	{
+		$constraints = [];
+		foreach( $array as $index )
+		{
+			if( isset( $_GET[$index] ) && $_GET[$index] !== '' )
+				$constraints[] =($table_name?$table_name.'.':''). $index.'="'.DBTable::escape( $_GET[ $index ]).'"';
+		}
+		return $constraints;
+	}
 
+	function getStartLikeConstraints( $array, $table_name)
+	{
+		error_log( json_encode(array_keys( $_GET )) );
+		$constraints = array();
+
+		foreach( $array as $index )
+		{
+			error_log( $index.'^' );
+			if( isset( $_GET[$index.'^'] ) && $_GET[$index.'^'] !== '' )
+			{
+				$constraints[] = ($table_name?$table_name.'.':'').$index.' LIKE "'.DBTable::escape($_GET[ $index.'^' ]).'%"';
+			}
+		}
+		if( count( $constraints ) )
+			return array( "(".join(' OR ',$constraints ).")" );
+		return array();
+	}
 	function getLikeConstraints($array,$table_name='')
 	{
 		$constraints = [];
@@ -54,7 +70,9 @@ class SuperRest extends \akou\RestController
 				$constraints[] = ($table_name?$table_name.'.':'').$index.' LIKE "%'.DBTable::escape($_GET[ $index.'~~' ]).'%"';
 			}
 		}
-		return $constraints;
+		if( count( $constraints ) )
+			return array( "(".join(' OR ',$constraints ).")" );
+		return array();
 	}
 
 	function getBiggerOrEqualThanConstraints( $array, $table_name )
@@ -121,7 +139,8 @@ class SuperRest extends \akou\RestController
 		$ge_constraints = $this->getBiggerOrEqualThanConstraints( $key_constraints, $table_name );
 		$smallest_than_constraints = $this->getSmallestThanConstraints( $key_constraints, $table_name );
 		$csv_constraints	= $this->getCsvConstraints( $key_constraints, $table_name );
-		return array_merge( $like_constraints, $equal_constrints, $ge_constraints, $smallest_than_constraints, $csv_constraints );
+		$start_constraints			= $this->getStartLikeConstraints( $key_constraints, $table_name );
+		return array_merge( $like_constraints, $equal_constrints, $ge_constraints, $smallest_than_constraints, $csv_constraints,$start_constraints );
 	}
 
 	function getSessionErrors($usuario,$roles = NULL )
